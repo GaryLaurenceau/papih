3897518ddbd44fdb8bb83f65a622e60b517b8970
Gary Laurenceau <gary.laurenceau@gmail.com>
2015-05-23 03:37:42 -0700

    comment postgresql settings in settings.py and requirements.txt, using sqlite instead

--- papih/settings.py
+++ papih/settings.py
@@ -69,14 +69,21 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'papih',
+#        'USER': 'postgres',
+#        'PASSWORD': 'slain28_pool',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'papih',
-        'USER': 'postgres',
-        'PASSWORD': 'slain28_pool',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
