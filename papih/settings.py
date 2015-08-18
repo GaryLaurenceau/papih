13764e731f07132471e403c54a2ac02b5d1c0b3c
Gary Laurenceau <gary.laurenceau@gmail.com>
2015-08-18 15:12:38 +0200

    add README

--- papih/settings.py
+++ papih/settings.py
@@ -74,16 +74,16 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'papih',
-#        'USER': 'postgres',
-#        'PASSWORD': 'slain28_pool',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-#    }
-#}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'papih',
+#         'USER': 'postgres',
+#         'PASSWORD': 'slain28_pool',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {
     'default': {
