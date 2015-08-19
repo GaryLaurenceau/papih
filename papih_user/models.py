af675a9e2fc2a8f5c33a43ee2a1043a7a14747f0
seth <seth@email.com>
2015-08-19 03:24:58 +0200

    merge done

--- papih_user/models.py
+++ papih_user/models.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -14,4 +8,3 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
->>>>>>> develop
